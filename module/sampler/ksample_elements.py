a253fdc8b3d80bf070fa453e3fe9479bacfcdef6
Ludobico <aqs450@gmail.com>
2025-03-14 17:34:04 +0900

    feat : create latent vector without denoising process

---
+++ module/sampler/ksample_elements.py
@@ -0,0 +1,78 @@
+from typing import Optional, List, Union
+import torch
+import inspect
+from module.tensor_utils import randn_tensor
+
+def retrieve_timesteps(
+    scheduler,
+    num_inference_steps: Optional[int] = None,
+    device: Optional[Union[str, torch.device]] = None,
+    timesteps: Optional[List[int]] = None,
+    sigmas: Optional[List[float]] = None,
+    **kwargs,
+):
+    if timesteps is not None and sigmas is not None:
+        raise ValueError("Only one of `timesteps` or `sigmas` can be passed. Please choose one to set custom values")
+    if timesteps is not None:
+        accepts_timesteps = "timesteps" in set(inspect.signature(scheduler.set_timesteps).parameters.keys())
+        if not accepts_timesteps:
+            raise ValueError(
+                f"The current scheduler class {scheduler.__class__}'s `set_timesteps` does not support custom"
+                f" timestep schedules. Please check whether you are using the correct scheduler."
+            )
+        scheduler.set_timesteps(timesteps=timesteps, device=device, **kwargs)
+        timesteps = scheduler.timesteps
+        num_inference_steps = len(timesteps)
+    elif sigmas is not None:
+        accept_sigmas = "sigmas" in set(inspect.signature(scheduler.set_timesteps).parameters.keys())
+        if not accept_sigmas:
+            raise ValueError(
+                f"The current scheduler class {scheduler.__class__}'s `set_timesteps` does not support custom"
+                f" sigmas schedules. Please check whether you are using the correct scheduler."
+            )
+        scheduler.set_timesteps(sigmas=sigmas, device=device, **kwargs)
+        timesteps = scheduler.timesteps
+        num_inference_steps = len(timesteps)
+    else:
+        scheduler.set_timesteps(num_inference_steps, device=device, **kwargs)
+        timesteps = scheduler.timesteps
+    return (timesteps, num_inference_steps)
+
+def get_guidance_scale_embedding(
+        w : torch.Tensor, embedding_dim : int = 512, dtype : torch.dtype = torch.float32
+) -> torch.Tensor:
+    assert len(w.shape) == 1
+
+    w = w * 1000.0
+
+    half_dim = embedding_dim // 2
+    emb = torch.log(torch.tensor(10000.0)) / (half_dim - 1)
+    emb = torch.exp(torch.arange(half_dim, dtype=dtype) * -emb)
+    emb = w.to(dtype)[:, None] * emb[None, :]
+    emb = torch.cat([torch.sin(emb), torch.cos(emb)], dim=1)
+
+    if embedding_dim % 2 == 1:
+        emb = torch.nn.functional.pad(emb, (0,1))
+    assert emb.shape == (w.shape[0], embedding_dim)
+    return emb
+
+def prepare_latents(batch_size, num_channels_latents, height, width, dtype, device, generator, vae_scale_factor):
+    shape = (
+        batch_size,
+        num_channels_latents,
+        int(height) // vae_scale_factor,
+        int(width) // vae_scale_factor,
+    )
+
+    if isinstance(generator, list) and len(generator) != batch_size:
+        raise ValueError(
+            f"You have passed a list of generators of length {len(generator)}, but requested an effective batch"
+            f" size of {batch_size}. Make sure the batch size matches the length of the generators."
+        )
+
+    latents = randn_tensor(shape, generator=generator, device=device, dtype=dtype)
+
+    # scale the initial noise by the standard deviation required by the scheduler
+    # ### Provides the initial latent vector without the denoising process ###
+    # latents = latents * scheduler.init_noise_sigma
+    return latents
